b2076c271da08b4ef198822494de493a682a21a5
Mike <assoune.mike@gmail.com>
2020-08-26 13:41:34 +0200

    c

--- index.js
+++ index.js
@@ -2,26 +2,22 @@ const
   io = require("socket.io"),
   server = io.listen(process.env.PORT);
 
-let
-  sequenceNumberByClient = new Map();
+const clients = {};
 
 // event fired every time a new client connects:
 server.on("connection", (socket) => {
   console.info(`Client connected [id=${socket.id}]`);
   // initialize this client's sequence number
-  sequenceNumberByClient.set(socket, 1);
+  clients[socket.id] = socket;
 
   // when socket disconnects, remove it from the list:
   socket.on("disconnect", () => {
-    sequenceNumberByClient.delete(socket);
+    delete clients[socket.id];
     console.info(`Client gone [id=${socket.id}]`);
   });
 });
 
 // sends each client its current sequence number
 setInterval(() => {
-  for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-    client.emit("seq-num", sequenceNumber);
-    sequenceNumberByClient.set(client, sequenceNumber + 1);
-  }
+  console.log(Object.keys(clients).length);
 }, 1000);
